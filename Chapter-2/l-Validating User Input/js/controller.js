9fdd6910daadb60d8faba5de4647954824eadb76
Jorge Zavala <jzavala@growthaccelerationpartners.com>
2017-09-10 21:33:56 -0600

    Chapter-2: Validating User Input

---
+++ Chapter-2/l-Validating User Input/js/controller.js
@@ -0,0 +1,8 @@
+
+angular.module('myApp', []).controller('AddUserController', function($scope) {
+	$scope.message = '';
+    $scope.addUser = function () {
+    // TODO for the reader: actually save user to database...
+    $scope.message = 'Thanks, ' + $scope.user.first + ', we added you!';
+    };
+});
